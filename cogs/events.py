d03b3cf9a7eb3e680f6c0aac37f16f738afd98fc
SaidBySolo <saidbysolo@gmail.com>
2020-05-11 11:53:32 +0900

    로그 추가

--- cogs/events.py
+++ cogs/events.py
@@ -10,5 +10,15 @@ class Events(commands.Cog):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("권한이 부족합니다.", delete_after=5)
         
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        channel = self.bot.get_channel(709231709392207953)
+        embed = discord.Embed(title="Log", description=f'``{message.author.id}``')
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="Name",value=message.Name)
+        embed.add_field(name="Timestamp",value=message.timestamp)
+        embed.add_field(name="Content", value=message.content)
+        await channel.send(embed=embed)
+        await bot.process_commands(message)
 def setup(bot):
     bot.add_cog(Events(bot))
